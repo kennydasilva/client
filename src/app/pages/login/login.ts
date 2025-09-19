4acbe945cc590247352853b6247842d160a1936c
Kenny Dasilva <kennydasilva542@gmail.com>
2025-09-19 13:18:47 +0200

    addiction of the login page form

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,17 @@
+import { MatIconModule } from '@angular/material/icon';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [MatInputModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+   hide=true;
 
 }
