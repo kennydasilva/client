58305f0c7ebacab859101f8639c5d3fac584059b
Kenny Dasilva <kennydasilva542@gmail.com>
2025-09-19 15:37:49 +0200

    update of the login page

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,17 +1,28 @@
 import { MatIconModule } from '@angular/material/icon';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-login',
-  imports: [MatInputModule, MatIconModule, ReactiveFormsModule],
+  imports: [RouterLink, MatInputModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
    hide=true;
+   form!:FormGroup;
+   fb = inject(FormBuilder);
 
+   login(){}
+
+   ngOnInit(): void{
+     this.form =this.fb.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required]],
+     })
+   }
 }
