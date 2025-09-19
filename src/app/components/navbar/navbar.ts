d1088e5a693654be6e99dfa9c5e7e05e9692bdc4
Kenny Dasilva <kennydasilva542@gmail.com>
2025-09-19 12:20:31 +0200

    addiction of the homepage

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
+
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+
+}
